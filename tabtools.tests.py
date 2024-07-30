199f286e93ab564a89e1a66e90b8999a85b632e1
Guido Draheim <guidod@gmx.de>
2024-07-30 20:24:17 +0200

    v1.1.3313

--- tabtools.tests.py
+++ tabtools.tests.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 __copyright__ = "(C) 2022-2024 Guido Draheim, licensed under the Apache License 2.0"""
-__version__ = "1.0.3313"
+__version__ = "1.1.3313"
 
 from tabtools import *
 import sys
