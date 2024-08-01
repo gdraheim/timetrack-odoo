e1094b20ef86d370e2e8923e215448d5df5ba24b
Guido Draheim <guidod@gmx.de>
2024-08-01 05:32:40 +0200

    v1.1.3321

--- zeit2jira.py
+++ zeit2jira.py
@@ -4,7 +4,7 @@ Synchronize odoo-import data (from zeit.txt) with Jira worklog entries.
 """
 
 __copyright__ = "(C) 2022-2024 Guido Draheim, licensed under the Apache License 2.0"""
-__version__ = "0.4.3314"
+__version__ = "0.4.3321"
 
 from typing import Optional, Union, Dict, List, Tuple, Iterable, Iterator, cast, NamedTuple
 
