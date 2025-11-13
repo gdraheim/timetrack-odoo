f7c1e92b636d929c34c6b3393fba42bedab957b2
Guido Draheim <guidod@gmx.de>
2025-11-13 09:10:44 +0100

    workbilling start

---
+++ workbilling.py
@@ -0,0 +1,7 @@
+
+#! /usr/bin/env python3
+# pylint: disable=missing-function-docstring,missing-class-docstring
+
+class WorkRecordForBilling:
+    def scanline(self, line: str) -> None:
+        pass
